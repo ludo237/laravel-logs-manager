8cb4cee1eacdca602a5e849989382e13090054a8
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2018-09-26 12:39:26 +0200

    Improve base command class with new logic

--- src/Console/BaseCommand.php
+++ src/Console/BaseCommand.php
@@ -4,6 +4,8 @@ namespace Ludo237\LogsManager\Console;
 
 use Illuminate\Console\Command;
 use Illuminate\Console\ConfirmableTrait;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Facades\File;
 
 /**
  * Class BaseCommand
@@ -28,10 +30,33 @@ abstract class BaseCommand extends Command
     public $logs;
     
     /**
+     * Set the current storagePath to either a custom one or a default from laravel
+     *
+     * @param \Illuminate\Filesystem\Filesystem $customStoragePath
+     * @return void
+     */
+    protected function setStoragePath(?Filesystem $customStoragePath) : void
+    {
+        $this->storagePath = $customStoragePath ?? storage_path("logs");
+    }
+    
+    /**
+     * Collect all the logs inside the storagePath variable that we have
+     *
+     * @return void
+     */
+    protected function collectLogsFile() : void
+    {
+        $this->logs = collect(File::files($this->storagePath));
+    }
+    
+    /**
      * Determine if the specified folder has logs in it
      * if not, the command will exit
+     *
+     * @return void
      */
-    protected function checkLogsPresence()
+    protected function checkLogsPresence() : void
     {
         if ($this->logs->isEmpty()) {
             $this->warn("Logs folder is empty");
@@ -41,10 +66,11 @@ abstract class BaseCommand extends Command
     }
     
     /**
-     * If a command need the confirmation this method
-     * will allow that
+     * If a command need the confirmation this method will allow that
+     *
+     * @return void
      */
-    protected function askForConfirmation()
+    protected function askForConfirmation() : void
     {
         if (!$this->confirmToProceed("There are {$this->logs->count()} files, do you want to remove them?")) {
             exit;
