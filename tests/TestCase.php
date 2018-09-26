a4e5b22106951dc50d596f100f92652aaed243c4
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2018-09-26 12:39:49 +0200

    Update TestCase for future tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,19 +2,17 @@
 
 namespace Ludo237\LogsManager\Tests;
 
+use Ludo237\LogsManager\LogsManagerServiceProvider;
+use Orchestra\Testbench\TestCase as OTestCase;
+
 /**
  * TestCase
  * @package Ludo237\LogsManager\Tests
  */
-abstract class TestCase extends \Orchestra\Testbench\TestCase
+abstract class TestCase extends OTestCase
 {
     protected function getPackageProviders($app)
     {
-        return [];
-    }
-
-    protected function getPackageAliases($app)
-    {
-        return [];
+        return [LogsManagerServiceProvider::class];
     }
 }
