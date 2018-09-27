6dd27d171c7771782f39dc0e2b5acd8e92d59a96
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2018-09-27 19:07:41 +0200

    Complete ArchiveCommand test 🤙🤙

--- tests/ArchiveCommandTest.php
+++ tests/ArchiveCommandTest.php
@@ -16,30 +16,62 @@ final class ArchiveCommandTest extends TestCase
         
         $this->artisan("log:archive")
             ->expectsOutput("logs_archive_{$now}.zip is filling...")
+            ->expectsOutput("Adding laravel_dummy_log_0.log to logs_archive_{$now}.zip")
+            ->expectsOutput("Adding laravel_dummy_log_4.log to logs_archive_{$now}.zip")
+            ->expectsOutput("logs_archive_{$now}.zip created!")
             ->assertExitCode(0);
         
+        $this->assertLogsFolderContainsZip();
     }
     
     /** @test */
-   public function it_displays_a_message_if_the_log_folder_is_empty()
+    public function it_displays_a_message_if_the_log_folder_is_empty()
     {
         $this->artisan("log:archive")
             ->expectsOutput("Logs folder is empty")
             ->assertExitCode(0);
+        
+        $this->assertLogsFolderDoesNotContainZip();
     }
     
+    /** @test */
     public function if_an_archive_with_the_same_name_is_already_present_it_quits()
     {
-    
+        $this->populateLogsFolder();
+        $now = now()->toDateString();
+        $path = storage_path("logs");
+        
+        $this->artisan("log:archive")->run();
+        
+        $this->artisan("log:archive")
+            ->expectsOutput("An archive called logs_archive_{$now}.zip already exists in {$path}");
     }
     
+    /** @test */
     public function it_accepts_a_name_for_customization()
     {
+        $this->populateLogsFolder();
     
+        $this->artisan("log:archive", ["name" => "foobar"])
+            ->expectsOutput("foobar.zip is filling...")
+            ->expectsOutput("Adding laravel_dummy_log_0.log to foobar.zip")
+            ->expectsOutput("Adding laravel_dummy_log_4.log to foobar.zip")
+            ->expectsOutput("foobar.zip created!")
+            ->assertExitCode(0);
+    
+        $this->assertLogsFolderContainsZip("foobar.zip");
     }
-    
+
+    /** @test */
     public function it_can_remove_an_archive()
     {
+        $this->populateLogsFolder();
+        $now = now()->toDateString();
+        $path = storage_path("logs");
     
+        $this->artisan("log:archive", ["--remove" => true])
+            ->expectsOutput("Archive logs_archive_{$now}.zip removed from {$path}");
+        
+        $this->assertLogsFolderDoesNotContainZip();
     }
 }
